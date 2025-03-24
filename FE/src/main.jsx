ebe17c76cd6602e4b484a36554bd9cef3cb2427d
rlawodnr1109 <wodnr1109@naver.com>
2025-03-24 15:38:48 +0900

    feat: add NotFoundPage

--- FE/src/main.jsx
+++ FE/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 
 import App from "./App.jsx";
+import { HomePage } from "./pages/HomePage/index.jsx";
+import { NotFoundPage } from "./pages/NotFoundPage/index.jsx";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
@@ -11,6 +13,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
         </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <App />
     </StrictMode>
