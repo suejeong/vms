a57ff187ce7952803b4df378aea97c19cf85b10e
Hoon.hs <49442904+az0319h@users.noreply.github.com>
2025-03-24 17:52:57 +0900

    프론트엔드 기본 구조
    Header를 포함한 react-router-dom관련 기능 구현

---
+++ FS-main/FE/src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './styles/global.css'
+import './styles/index.css'
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App";
+import { HomePage } from "./pages/HomePage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import Compare from "./pages/Compare";
+import Status from "./pages/Status";
+import Investment from "./pages/Investment";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundPage />, // 에러 페이지 설정
+    children: [
+      { index: true, element: <HomePage /> }, // 기본 페이지
+      { path : '/compare', element : <Compare />},
+      { path : '/status', element : <Status />},
+      { path : '/investment', element : <Investment />},
+    ],
+  },
+]);
+
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ FS-main/FE/src/pages/NotFoundPage/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NotFoundPage = () => {
+  return (
+    <section>
+      엥? 이런 페이지는 없어요..
+    </section>
+  )
+};
