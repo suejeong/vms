cbccc48ebd84bd36e81ff11706b4c53cfc2d9107
rlawodnr1109 <wodnr1109@naver.com>
2025-03-27 10:34:10 +0900

    feat: modify mockData and OrderBy component for reusability

--- FE/src/components/ComparisonResultPage/OrderBy/OrderBy.jsx
+++ FE/src/components/ComparisonResultPage/OrderBy/OrderBy.jsx
@@ -1,9 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./OrderBy.module.css";
 import ic_arrow_top from "../../../assets/arrow_top.png";
 import ic_arrow_arrow_bottom from "../../../assets/arrow_bottom.png";
 
-export const OrderBy = ({ currentState, handleOrderChange }) => {
+export const OrderBy = ({
+  listState,
+  setListState,
+  currentState,
+  handleOrderChange,
+}) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleDropdown = () => {
@@ -14,6 +19,41 @@ export const OrderBy = ({ currentState, handleOrderChange }) => {
     handleOrderChange(value);
     setIsOpen(false);
   };
+
+  const sortCompanies = (orderBy) => {
+    let sortedList = [...listState];
+
+    switch (orderBy) {
+      case "누적 투자금액 높은순":
+        sortedList.sort((a, b) => b.total_investment - a.total_investment);
+        break;
+      case "누적 투자금액 낮은순":
+        sortedList.sort((a, b) => a.total_investment - b.total_investment);
+        break;
+      case "매출액 높은순":
+        sortedList.sort((a, b) => b.total_profit - a.total_profit);
+        break;
+      case "매출액 낮은순":
+        sortedList.sort((a, b) => a.total_profit - b.total_profit);
+        break;
+      case "고용 인원 많은순":
+        sortedList.sort((a, b) => b.employee_count - a.employee_count);
+        break;
+      case "고용 인원 적은순":
+        sortedList.sort((a, b) => a.employee_count - b.employee_count);
+        break;
+      default:
+        break;
+    }
+
+    setListState(sortedList);
+  };
+
+  useEffect(() => {
+    if (currentState) {
+      sortCompanies(currentState);
+    }
+  }, [currentState]);
 
   return (
     <div className={styles.dropdownContainer}>
@@ -27,22 +67,18 @@ export const OrderBy = ({ currentState, handleOrderChange }) => {
       </div>
       {isOpen && (
         <ul className={styles.dropdownMenu}>
-          <li onClick={() => handleSelect("total_investment_desc")}>
+          <li onClick={() => handleSelect("누적 투자금액 높은순")}>
             누적 투자금액 높은순
           </li>
-          <li onClick={() => handleSelect("total_investment_asc")}>
+          <li onClick={() => handleSelect("누적 투자금액 낮은순")}>
             누적 투자금액 낮은순
           </li>
-          <li onClick={() => handleSelect("total_profit_desc")}>
-            매출액 높은순
-          </li>
-          <li onClick={() => handleSelect("total_profit_asc")}>
-            매출액 낮은순
-          </li>
-          <li onClick={() => handleSelect("employee_count_desc")}>
+          <li onClick={() => handleSelect("매출액 높은순")}>매출액 높은순</li>
+          <li onClick={() => handleSelect("매출액 낮은순")}>매출액 낮은순</li>
+          <li onClick={() => handleSelect("고용 인원 많은순")}>
             고용 인원 많은순
           </li>
-          <li onClick={() => handleSelect("employee_count_asc")}>
+          <li onClick={() => handleSelect("고용 인원 적은순")}>
             고용 인원 적은순
           </li>
         </ul>
