2b11fd7a42bc7b1f530c67ab76bd8a0c79eabf8f
suejeong <dglogpic@gmail.com>
2025-04-01 11:46:07 +0900

    검색창 컴포넌트

--- FE/src/components/Title/Title.jsx
+++ FE/src/components/Title/Title.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Title.module.scss'
 
+// 기업리스트, 투자현황, 나의 기업 비교 등 페이지의 타이틀 메시지를 받아오는 컴포넌트입니다.
 function Title({ text }) {
   return (
     <h2 className={styles.h2Title}>{text}</h2>
