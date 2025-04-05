849c3d69b960f2fbe1088d0a9ad2bbf9f99ebaac
suejeong <dglogpic@gmail.com>
2025-04-05 13:47:47 +0900

    CompanyInfo 정렬 CSS 수정

--- FE/src/components/ComparePage/CompanyInfo/CompanyInfo.jsx
+++ FE/src/components/ComparePage/CompanyInfo/CompanyInfo.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import style from "./CompanyInfo.module.scss";
 
-function CompanyInfo({ company }) {
+function CompanyInfo({ company, direction = "row"  }) {
   return (
-    <>
-      <img src={`/images/companies/${company.name}.png`} alt="company" />
-      <p className={style.companyName}>{company.name}</p>
-      <p className={style.companyCategory}>{company.category}</p>
-    </>
+    <div  className={`${style.companyWrapper} ${style[direction]}`}>
+      <img 
+        src={`/images/companies/${company.name}.png`} 
+        alt="company" 
+        className={style.companyLogo}
+      />
+      <div className={style.companyText}>
+        <p className={style.companyName}>{company.name}</p>
+        <p className={style.companyCategory}>{company.category}</p>
+      </div>
+    </div>
   );
 }
 
