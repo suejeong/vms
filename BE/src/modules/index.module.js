e6ceb1fdfc0b9eed3021df92dd64d3345abb7d74
rlawodnr1109 <wodnr1109@naver.com>
2025-04-07 10:42:39 +0900

    refactor: Delete lists.module

--- BE/src/modules/index.module.js
+++ BE/src/modules/index.module.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const companyRouter = require("./company.module");
-const listsRouter = require("./lists.module");
 const investRouter = require("./invest.module");
 
 const router = express.Router();
 
 router.use("/company", companyRouter);
-router.use("/lists", listsRouter);
 router.use("/invest", investRouter);
 module.exports = router;
