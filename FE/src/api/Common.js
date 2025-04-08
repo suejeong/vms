264e14a5873b070a388fbac1ac49459ddf0f47ae
shimyubin <tla1161@naver.com>
2025-04-08 17:44:51 +0900

    switch rander

--- FE/src/api/Common.js
+++ FE/src/api/Common.js
@@ -6,10 +6,10 @@ const VITE_BASE_URL = import.meta.env.VITE_BASE_URL;
 // 인스턴스
 export const instance = axios.create({
   //배포 환경
-  // baseURL: VITE_API_URL,
+  baseURL: VITE_API_URL,
 
   //로컬 환경
-  baseURL: VITE_BASE_URL,
+  //baseURL: VITE_BASE_URL,
 });
 
 // 에러 처리
