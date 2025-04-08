c750f9d427e61045dd97856f1087eccd6e5560bc
subinkim9755 <82797486+subinkim9755@users.noreply.github.com>
2025-04-08 13:57:35 +0900

    Update BoardList.jsx

--- FE/src/components/BoardList/BoardList.jsx
+++ FE/src/components/BoardList/BoardList.jsx
@@ -39,7 +39,9 @@ export default function BoardList({
                 let value = company[field];
 
                 if (
-                  ["viewTotalInvestAmount", "totalInvestment"].includes(field)
+                  ["viewTotalInvestAmount",
+                    "totalInvestment",
+                    "totalProfit",].includes(field)
                 ) {
                   value = ChangeToNumber(value);
                 }
